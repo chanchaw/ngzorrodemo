b9507a2e521430752e1d2c7f32550babdb3e61a5
chanchaw <chan_chaw@126.com>
2020-05-18 15:23:24 +0800

    解决按钮不可用的问题

--- src/app/pages/button/button.component.ts
+++ src/app/pages/button/button.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
 
+  disabled1:boolean = false;
   btnDisabled:boolean = false;
 
   constructor() { }
